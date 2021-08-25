0eb123b19e9f53f6b11a82b9c2f5ce1b55c2b14b
Sohan Vikas Patil <sohanpatil2109@gmail.com>
2021-08-25 08:45:26 +0530

    Updated alphabetDetection.py

--- alphabetDetection.py
+++ alphabetDetection.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import pandas as pd
+from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
@@ -13,7 +14,8 @@ if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
     ssl._create_default_https_context = ssl._create_unverified_context
 
 X = np.load('image.npz')['arr_0']
-y = pd.read_csv('labels.csv')['labels']
+y = pd.read_csv("labels.csv")["labels"]
+# X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
 print(pd.Series(y).value_counts())
 
 classes = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
@@ -32,44 +34,45 @@ print("The accuracy is :- ",accuracy)
 cap = cv2.VideoCapture(0)
 
 while(True):
-  try:
-    ret, frame = cap.read()
+    try:
+        ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    height, width = gray.shape
-    upper_left = (int(width / 2 - 56), int(height / 2 - 56))
-    bottom_right = (int(width / 2 + 56), int(height / 2 + 56))
+        height, width = gray.shape
+        upper_left = (int(width / 2 - 56), int(height / 2 - 56))
+        bottom_right = (int(width / 2 + 56), int(height / 2 + 56))
 
-    cv2.rectangle(gray, upper_left, bottom_right, (0, 255, 0), 2)
+        cv2.rectangle(gray, upper_left, bottom_right, (0, 255, 0), 2)
 
-    roi = gray[upper_left[1]:bottom_right[1], upper_left[0]:bottom_right[0]]
+        roi = gray[upper_left[1]:bottom_right[1], upper_left[0]:bottom_right[0]]
 
-    im_pil = Image.fromarray(roi)
+        im_pil = Image.fromarray(roi)
 
-    image_bw = im_pil.convert('L')
-    image_bw_resized = image_bw.resize((28,28), Image.ANTIALIAS)
+        image_bw = im_pil.convert('L')
+        image_bw_resized = image_bw.resize((28,28), Image.ANTIALIAS)
 
-    image_bw_resized_inverted = PIL.ImageOps.invert(image_bw_resized)
-    pixel_filter = 20
+        image_bw_resized_inverted = PIL.ImageOps.invert(image_bw_resized)
+        pixel_filter = 20
 
-    min_pixel = np.percentile(image_bw_resized_inverted, pixel_filter)
-    image_bw_resized_inverted_scaled = np.clip(image_bw_resized_inverted-min_pixel, 0, 255)
+        min_pixel = np.percentile(image_bw_resized_inverted, pixel_filter)
+        image_bw_resized_inverted_scaled = np.clip(image_bw_resized_inverted-min_pixel, 0, 255)
 
-    max_pixel = np.max(image_bw_resized_inverted)
-    image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled) / max_pixel
+        max_pixel = np.max(image_bw_resized_inverted)
+        image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled) / max_pixel
+    
+        test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,784)
+        test_pred = clf.predict(test_sample)
+        print("Predicted class is: ", test_pred)
 
-    test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,784)
-    test_pred = clf.predict(test_sample)
-    print("Predicted class is: ", test_pred)
+        cv2.imshow('frame', gray)
 
-    cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
-
-  except Exception as e:
-    pass
+    except Exception as e:
+        print("Error occured", e)  
+        pass
 
 cap.release()
 cv2.destroyAllWindows()
